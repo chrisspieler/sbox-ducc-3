53d9d9f6ee46797868d8aaba49da1dc45aa6a96b
ducc <spielerc@gmail.com>
2024-05-07 05:00:26 -0600

    First commit

---
+++ Libraries/facepunch.playercontroller/Code/PlayerFootsteps.cs
@@ -0,0 +1,53 @@
+
+using Sandbox.Audio;
+
+public sealed class PlayerFootsteps : Component
+{
+	[Property] SkinnedModelRenderer Source { get; set; }
+	[Property] MixerHandle TargetMixer { get; set; }
+	[Property] public float VolumeScale { get; set; } = 1f;
+
+	protected override void OnEnabled()
+	{
+		if ( Source is null )
+			return;
+
+		Source.OnFootstepEvent += OnEvent;
+	}
+
+	protected override void OnDisabled()
+	{
+		if ( Source is null )
+			return;
+
+		Source.OnFootstepEvent -= OnEvent;
+	}
+
+	TimeSince timeSinceStep;
+
+	private void OnEvent( SceneModel.FootstepEvent e )
+	{
+		if ( timeSinceStep < 0.2f )
+			return;
+
+		var tr = Scene.Trace
+			.Ray( e.Transform.Position + Vector3.Up * 20, e.Transform.Position + Vector3.Up * -20 )
+			.Run();
+
+		if ( !tr.Hit )
+			return;
+
+		if ( tr.Surface is null )
+			return;
+
+		timeSinceStep = 0;
+
+		var sound = e.FootId == 0 ? tr.Surface.Sounds.FootLeft : tr.Surface.Sounds.FootRight;
+		if ( sound is null ) return;
+
+		var handle = Sound.Play( sound, tr.HitPosition + tr.Normal * 5 );
+		handle.Volume *= e.Volume * VolumeScale;
+		handle.TargetMixer = TargetMixer.Get();
+		handle.Update();
+	}
+}
---
+++ code/Quacker.cs
@@ -0,0 +1,15 @@
+﻿namespace Ducc;
+
+public sealed class Quacker : Component
+{
+	[Property] public SoundPointComponent SoundPoint { get; set; }
+
+	protected override void OnUpdate()
+	{
+		if ( Input.Pressed( "attack1" ) )
+		{
+			SoundPoint.StopSound();
+			SoundPoint.StartSound();
+		}
+	}
+}
