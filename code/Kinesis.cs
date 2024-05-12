a5c4928db8fcaeb609a0841d56e9e860be2f3fde
ducc <spielerc@gmail.com>
2024-05-12 16:10:29 -0600

    Mites crawl on walls and ceiling

--- code/Kinesis.cs
+++ code/Kinesis.cs
@@ -12,6 +12,9 @@ namespace Sandbox
 		private float _comfort;
 		[Property] public float Speed => _speed;
 		private float _speed;
+		[Property] public float TraceHeight { get; set; } = 0.5f;
+		[Property] public float TraceForwardDistance { get; set; } = 1f;
+		[Property] public float TraceDownDistance { get; set; } = 0.5f;
 		private float _noiseSeed;
 
 		protected override void OnStart()
@@ -24,8 +27,12 @@ namespace Sandbox
 			UpdateComfort();
 			UpdateSpeed();
 			Transform.Rotation *= Rotation.FromAxis( Transform.Rotation.Up, GetRotation() );
-			Transform.Position += Transform.Rotation.Forward * _speed * Time.Delta;
-			// TODO: Cling to walls, snap to surface.
+			if ( !Transform.Rotation.Forward.IsNaN )
+			{
+				Transform.Position += Transform.Rotation.Forward * _speed * Time.Delta;
+			}
+			SnapForward();
+			SnapToFloor();
 		}
 
 		private void UpdateComfort()
@@ -49,6 +56,48 @@ namespace Sandbox
 			var noise = Noise.Perlin( Time.Now * 100, _noiseSeed ) * 2 - 1;
 			degrees *= noise;
 			return degrees * Time.Delta;
+		}
+
+		private void SnapForward()
+		{
+			var startPos = Transform.Position + Transform.Rotation.Up * TraceHeight;
+			var ray = new Ray( startPos, Transform.Rotation.Forward );
+			var tr = Scene.Trace
+				.Ray( ray, TraceForwardDistance )
+				.WithoutTags( "player" )
+				.Run();
+			if ( !tr.Hit )
+				return;
+
+			Transform.Position = tr.EndPosition;
+			var crossAxis = Transform.Rotation.Left.Cross( tr.Normal );
+			Transform.Rotation = Rotation.LookAt( crossAxis, tr.Normal );
+		}
+
+		private void SnapToFloor()
+		{
+			var startPos = Transform.Position + Transform.Rotation.Up * TraceHeight;
+			var ray = new Ray( startPos, Transform.Rotation.Down );
+			var tr = Scene.Trace
+				.Ray( ray, TraceDownDistance )
+				.WithoutTags( "player" )
+				.Run();
+
+			if ( !tr.Hit )
+			{
+				WrapAroundSnap();
+				return;
+			}
+
+			Transform.Position = tr.EndPosition;
+			Transform.Rotation = Rotation.LookAt( Transform.Rotation.Left.Cross( tr.Normal ), tr.Normal );
+		}
+
+		private void WrapAroundSnap()
+		{
+			// Just turn around instead of attempting to wrap around.
+			Transform.Rotation *= Rotation.FromYaw( 180f );
+			// TODO: Handle wrapping around over the lip of an edge.
 		}
 	}
 }
